0b9795ac785e2ec9b78542ed1613d8d90e1c0a87
naresh0458 <89672433+naresh0458@users.noreply.github.com>
2021-11-21 09:04:55 +0530

    Update example8.java

--- Logical-Programming/example8.java
+++ Logical-Programming/example8.java
@@ -6,10 +6,10 @@ public class VowCons {
 		System.out.println("enter a Character");
 		char ch=s.next().charAt(0);
 		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
-			System.out.println("Given character is vowel");
+			System.out.println(ch+" is a vowel");
 		}
 		else {
-			System.out.println("Given year is not a consonent");
+			System.out.println(ch+" is a consonent");
 		}
 	}
 }
