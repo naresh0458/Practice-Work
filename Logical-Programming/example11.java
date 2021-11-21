d03fdbce4bb7f02e4cec33a8739ba05e4cab997a
naresh0458 <89672433+naresh0458@users.noreply.github.com>
2021-11-21 09:22:05 +0530

    Update example11.java

--- Logical-Programming/example11.java
+++ Logical-Programming/example11.java
@@ -7,29 +7,21 @@ public class example11 {
 	    System.out.println("enter a week number ");
 	    int num=scan.nextInt();
 	    
-	    if(num==0){
-	        System.out.println("sunday");
-	    }
-	    else if(num==1){
-	         System.out.println("monday");
-	    }
-	    else if(num==3){
-	         System.out.println("tuesday");
-	    }
-	    else if(num==4){
-	         System.out.println("wednesday");
-	    }
-	    else if(num==5){
-	         System.out.println("thursday");
-	    }
-	    else if(num==6){
-	         System.out.println("friday");
-	    }
-	    else if(num==7){
-	         System.out.println("saturday");
-	    }
-	    else{
-	        System.out.println("enter valid week number");
-	    }
+	    if(num==0)
+			System.out.println("sunday");
+		else if(num==1)
+			System.out.println("monday");
+		else if(num==2)
+			System.out.println("tuesday");
+		else if(num==3)
+			System.out.println("wednesday");
+		else if(num==4)
+			System.out.println("thursday");
+		else if(num==5)
+			System.out.println("friday");
+		else if(num==6)
+			System.out.println("saturday");
+		else
+			System.out.println("please select week number inbetween '0' and '6' ");
 }
 }
